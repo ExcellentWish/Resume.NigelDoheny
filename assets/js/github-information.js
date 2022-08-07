f2c28e617d97a141f17570673d79624956830bbc
Nigel Doheny <nigel.doheny93@gmail.com>
2022-08-07 11:23:59 +0000

    Add promises and error handling

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -10,4 +10,24 @@ function fetchGitHubInformation(event){
         <img src="assets/css/loader.gif" alt ="loading"/>
     </div>`)
     // everything up will create a loading animation 
+    // Below is promises 
+    $.when(
+        $.getJson(`https://api.github.com/${username}`)
+     ).then(
+        function(response){
+            var userData = response;
+            $("#gh-user-data").html(userInformationHTML(userData));
+        }, function(erroResponse){
+            if(erroResponse.status === 404){
+
+            $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+
+            }else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2> Error: ${erroResponse.responseJSON.message}</h2>`
+                )
+            }
+        }
+    )
 }
